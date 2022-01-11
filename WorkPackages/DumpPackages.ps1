3333d7d482a9b692ff653496c3ded4d6608910af
Some Guy <calculuswhiz@gmail.com>
2022-01-11 15:25:20 -0500

    Decided to use un-archived packages

--- WorkPackages/DumpPackages.ps1
+++ WorkPackages/DumpPackages.ps1
@@ -6,5 +6,5 @@ if (Test-Path $archiveName)
 }
 Copy-Item ..\packages\**\*.nupkg -Destination .\NugetDump
 # Change this if running at work:
-& 'D:\Program Files (D drive)\7-Zip\7z.exe' a $archiveName .\NugetDump\*.nupkg
+# & 'D:\Program Files (D drive)\7-Zip\7z.exe' a $archiveName .\NugetDump\*.nupkg
 Write-Host "Done"
