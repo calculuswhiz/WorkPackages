6d2348cf1972464560c22648b234cf2f6fbc8a72
Some Guy <calculuswhiz@gmail.com>
2022-01-11 13:19:48 -0500

    Added archive

---
+++ WorkPackages/DumpPackages.ps1
@@ -0,0 +1,10 @@
+$archiveName = ".\ark.7z"
+
+if (Test-Path $archiveName)
+{
+    Remove-Item $archiveName
+}
+Copy-Item ..\packages\**\*.nupkg -Destination .\NugetDump
+# Change this if running at work:
+& 'D:\Program Files (D drive)\7-Zip\7z.exe' a $archiveName .\NugetDump\*.nupkg
+Write-Host "Done"
